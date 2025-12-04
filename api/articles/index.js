449b7315d34f6cd57ec1437b0e7065e99d1c3e21
Alizamin Fatullayev <alizaminfatullayev06@gmail.com>
2025-12-04 14:09:25 +0400

    Fix API import paths

--- api/articles/index.js
+++ api/articles/index.js
@@ -1,5 +1,5 @@
-import { fetchLatestArticles, fetchArticlesByCategory } from './_lib/devto.js';
-import { getCacheKey, isCacheValid, getCache, setCache } from './_lib/cache.js';
+import { fetchLatestArticles, fetchArticlesByCategory } from '../_lib/devto.js';
+import { getCacheKey, isCacheValid, getCache, setCache } from '../_lib/cache.js';
 
 export default async function handler(req, res) {
   // Enable CORS
